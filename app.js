cfb76c830bcc5f8e7d66a466557261cdcc132fac
alan macdonald <alan macdonald>
2016-09-15 22:07:19 -0400

    another test

--- app.js
+++ app.js
@@ -30,11 +30,12 @@ var listReviewsRouter = require('./app/routes/listReviewsRouter');
 var app = express();
 
 // view engine setup
-//app.set('views', path.join(__dirname, 'views'));
-app.set('views', path.join(__dirname, '/views'));
+//app.set('views', path.join(__dirname, 'views')); original
+//app.set('views', path.join(__dirname, '/views'));
 //app.use(express.static(__dirname + '../public/app'));
-//app.set('views','./public/app/');
-app.set('view engine', 'jade');
+app.set('views','./public/app/');
+//app.set('view engine', 'jade');
+app.set('view engine', 'html'); original
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
