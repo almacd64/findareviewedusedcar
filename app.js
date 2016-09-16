5335f9fdc6041ef505e5950917fa5843cb421a60
alan macdonald <alan macdonald>
2016-09-15 22:30:09 -0400

    another test

--- app.js
+++ app.js
@@ -32,19 +32,8 @@ var app = express();
 // view engine setup
 
 app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
-//app.set('views', path.join(__dirname, '/views'));
-//app.use(express.static(__dirname + '../public/app'));
-//app.set('views','./public/app/');
-
-//app.set('view engine', 'jade');
-
-
-app.engine('html', require('jade').renderFile);
-app.set('view engine', 'html');
-
-
-//app.set('view engine', 'html'); 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
