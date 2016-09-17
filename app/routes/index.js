87ca5a4d529f337e2a9572210d6c384ad360b133
alan macdonald <alan macdonald>
2016-09-16 21:42:18 -0400

    another test

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,10 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    console.log("getting home page"+__dirname);
+    console.log("getting home pag e"+__dirname);
  // res.render('index', { title: 'Express' });
-    // res.sendFile(__dirname + "/index.html" );
-    res.sendFile("app/views/index.html");
+     res.sendFile(__dirname + "/index.html" );
+    //res.sendFile("app/views/index.html");
 });
 
 module.exports = router;
