88a576f6ec6789e800f7040634f0ad627ddf99b5
alan macdonald <alan macdonald>
2016-09-22 19:03:06 -0400

    test

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
     'secretKey': '12345-67890-09876-54321',
    // 'mongoUrl' : 'mongodb://localhost:27017/usedcarreview',
    //   'mongoUrl' : 'mongodb://heroku_29pl2t0w:partick1@ds029426.mlab.com:29426/heroku_29pl2t0w',
-     'mongoUrl' : 'mongodb://almacd:partick1@ds029426.mlab.com:29426/heroku_29pl2t0w',
+     'mongoUrl' : 'mongodb://almacd:partick1@ds035806.mlab.com:35806/heroku_cq2n498p',
     'facebook':{
         clientID: '1777957592483055',
         clientSecret: '7e201f9368bf903b8ebe241ddca3452f',
